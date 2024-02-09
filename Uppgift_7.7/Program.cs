54d61aaf47aee29909c21fa49348d7fc6d242974
ArvidUnell <arun06001@utb.vaxjo.se>
2024-02-09 10:37:21 +0100

    Add project files.

---
+++ Uppgift_7.7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Uppgift_7_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strängar1 = { "a", "b", "b", "c", "hej" };
+            string[] strängar2 = { "hej", "a", "a", "b", "programmering" };
+
+            foreach (string utSträng in FinnsIBåda(strängar1,strängar2))
+            {
+                Console.WriteLine(utSträng);
+            }
+        }
+        static string[] FinnsIBåda(string[] arr1, string[] arr2)
+        {
+            List<string> lista = new List<string>();
+
+            foreach (string sträng in arr1)
+            {
+                if (arr2.Contains(sträng) && !lista.Contains(sträng))
+                {
+                    lista.Add(sträng);
+                }
+            }
+            return lista.ToArray();
+        }
+    }
+}
